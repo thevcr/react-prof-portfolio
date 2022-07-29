b1556543fe6e0241906cb5ce2561185199932d75
thevcr <thevcr@gmail.com>
2022-07-29 08:51:22 -0400

    Updates

---
+++ precache-manifest.cc5d620957a27a698ba394ab3110ee93.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "644751e38b220a4c9a08",
+    "url": "/react-prof-portfolio/static/css/main.23ed1061.chunk.css"
+  },
+  {
+    "revision": "644751e38b220a4c9a08",
+    "url": "/react-prof-portfolio/static/js/main.407f907f.chunk.js"
+  },
+  {
+    "revision": "d28ad4a1abca5dd2c4a1",
+    "url": "/react-prof-portfolio/static/js/runtime~main.9b050f02.js"
+  },
+  {
+    "revision": "bd3cff99127d637ea0ed",
+    "url": "/react-prof-portfolio/static/css/2.97203420.chunk.css"
+  },
+  {
+    "revision": "bd3cff99127d637ea0ed",
+    "url": "/react-prof-portfolio/static/js/2.3bbd4436.chunk.js"
+  },
+  {
+    "revision": "af8870e8e5ad21c0bb44",
+    "url": "/react-prof-portfolio/static/js/3.789e687e.chunk.js"
+  },
+  {
+    "revision": "bae025c085635f622600aa8b12294ecb",
+    "url": "/react-prof-portfolio/static/media/wave-emoji.bae025c0.png"
+  },
+  {
+    "revision": "455e345cae61e8f56bc9a2c5d53c4a5c",
+    "url": "/react-prof-portfolio/static/media/vent-app-preview.455e345c.png"
+  },
+  {
+    "revision": "a3d82cd411f1f6b4d3b1f865b5328533",
+    "url": "/react-prof-portfolio/static/media/weather-by-day.a3d82cd4.png"
+  },
+  {
+    "revision": "c216a08f1e60dd4a0a60ce1a42c76fce",
+    "url": "/react-prof-portfolio/static/media/coding-quiz-screenshot.c216a08f.png"
+  },
+  {
+    "revision": "d62d97b433b466f278263e74d092e1be",
+    "url": "/react-prof-portfolio/static/media/password-gen.d62d97b4.png"
+  },
+  {
+    "revision": "e8d26d6fe92630ab64d5841eca2ac4f1",
+    "url": "/react-prof-portfolio/static/media/watchlist-app-screenshot.e8d26d6f.png"
+  },
+  {
+    "revision": "daf11f3294561fe11ca89d0c6ae6b81b",
+    "url": "/react-prof-portfolio/static/media/horiseon-web-screenshot.daf11f32.png"
+  },
+  {
+    "revision": "52e52a7c6524c65ba1dbc158a01c5e45",
+    "url": "/react-prof-portfolio/index.html"
+  }
+];
